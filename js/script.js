0067bfdccd49a877e4cfcb8eb90cfc3a8248a09d
LucasHenriqueSL <lucashenrique8736@gmail.com>
2022-10-07 12:06:32 -0300

    60%

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const pokemonName = document.querySelector('pokemon_name');
+
+
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  const data = await APIResponsesponse.json();
+  return data;
+}
+
+const renderPokemon = async (pokemon) =>{
+    const data = await fetchPokemon(pokemon);
+
+    pokemonName.innerHTML = data.name;
+    
+}
+
